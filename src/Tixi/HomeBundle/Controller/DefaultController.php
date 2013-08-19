9657dafcf0ff92007416b8db78a2fe44642cae90
Martin Jonasse <jonasse@datazug.ch>
2013-08-19 21:03:30 +0200

    Added all URLs and function points to the home page. Unit testing completed. todos documented in code.

--- src/Tixi/HomeBundle/Controller/DefaultController.php
+++ src/Tixi/HomeBundle/Controller/DefaultController.php
@@ -8,13 +8,21 @@ class DefaultController extends Controller
 {
     public function indexAction($slug = '')
     {
-     // parameters for viewing
+     // set filter using request parameters (carry over in session)
+        if (isset($_REQUEST['filter'])) { $filter = $_REQUEST['filter']; }
+        else {$filter = "Filter..."; };
+
+     // parameters for viewing (constants)
         $application = 'iTixi';     // @todo: get this from session (conditional)
         $version = '2.0.4';         // @todo: get this from session (conditional)
-        $customer = 'undefined';    // @todo: get this from session (conditional)
-        $breadcrumbs = 'Startseite';
+        $customer = 'Undefined';    // @todo: get this from session (conditional)
+        $breadcrumbs = 'Breadcrumbs (@todo)';
         $username = 'Anonym';       // @todo: get this from session (conditional)
         $subject = 'Startseite für Benutzer Anonym';
+        $baseurl = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME'];
+
+     // mode = 'edit' ¦ 'read'
+        $mode = 'read';             // @todo: get this from session (conditional)
 
      // parameters for links
         $request = $this->container->get('request');
@@ -24,8 +32,9 @@ class DefaultController extends Controller
         return $this->render(
             'TixiHomeBundle:Default:index.html.twig',
             array('application' => $application, 'version' => $version, 'customer' => $customer,
-                'breadcrumbs' => $breadcrumbs, 'username' => $username, 'subject' => $subject,
-                'routeid' => $routeid)
+                  'breadcrumbs' => $breadcrumbs, 'username' => $username, 'subject' => $subject,
+                  'filter' => $filter, 'baseurl' => $baseurl, 'mode' => $mode,
+                  'routeid' => $routeid)
         );
     }
 }
--- src/Tixi/HomeBundle/Resources/views/Default/menu.html.php
+++ src/Tixi/HomeBundle/Resources/views/Default/menu.html.php
@@ -6,7 +6,6 @@
  * Time: 10:00
  **/
 // src/Tixi/HomeBundle/Resources/views/Default/menu.html.php
-// @todo: MENU-BAR: add function points (anchors)
 
 // prefix
 echo '<div id="menu-bar">';
@@ -18,8 +17,9 @@ $items = count($menuitems);
 
 //loop
 foreach ($menuitems as $key => $menuitem):
- // calculate fully qualified URL
+ // calculate fully qualified URL and ID
     $route = $myurl.$menuitem["URL"];
+    $id = 'fpm'.str_replace('/', '_', $menuitem["URL"] );
 
  // calculate level changes: down, same, up
     $thislevel = substr_count($menuitem["URL"],'/');
@@ -29,12 +29,12 @@ foreach ($menuitems as $key => $menuitem):
 
  // build list elements
     if ($nextlevel == $thislevel) {
-        echo '<li><a href="'.$route.'">'.$menuitem["CAPTION"].'</a></li>'; // same
+        echo '<li><a href="'.$route.'" id="'.$id.'">'.$menuitem["CAPTION"].'</a></li>'; // same
     } elseif ($nextlevel > $thislevel) {
-        echo '<li class="dropdown"><a href="'.$route.'">'.$menuitem["CAPTION"].'</a>'; // down
+        echo '<li class="dropdown"><a href="'.$route.'" id="'.$id.'">'.$menuitem["CAPTION"].'</a>'; // down
         echo '<ul>'; // down
     } elseif ($nextlevel < $thislevel) {
-        echo '<li><a href="'.$route.'">'.$menuitem["CAPTION"].'</a></li>'; // up
+        echo '<li><a href="'.$route.'" id="'.$id.'">'.$menuitem["CAPTION"].'</a></li>'; // up
         for ($i = 1; $i <= ($thislevel - $nextlevel); $i++) {
             echo '</ul></li>'; // up
         };
