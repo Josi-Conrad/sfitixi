a5b5dc9fde1936cfa371252ac91f9bf772c067ad
Martin Jonasse <jonasse@datazug.ch>
2013-08-21 21:31:35 +0200

    Added /about page, added tixi_homepage_service, fixed _route problem in Twig. Unit testing completed.

--- src/Tixi/HomeBundle/Controller/DefaultController.php
+++ src/Tixi/HomeBundle/Controller/DefaultController.php
@@ -8,35 +8,17 @@ class DefaultController extends Controller
 {
     public function indexAction($slug = '')
     {
-     // set common parameters
-        $baseurl = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']; // root url
+     // called for all /home requests
 
-     // set parameters for the top frame
-        $application = 'iTixi';     // @todo: get this from session (conditional)
-        $version = '2.0.4';         // @todo: get this from session (conditional)
-        $customer = 'Undefined';    // @todo: get this from session (conditional)
-        $breadcrumbs = 'Breadcrumbs (@todo)';
-        $username = 'Anonym';       // @todo: get this from session (conditional)
-
-     // set parameters for the menubar
-
-     // set parameters for the subject
-        $subject = 'Startseite für Benutzer Anonym';
-
-     // set parameters for the taskbar
-        if (isset($_REQUEST['filter'])) { $filter = $_REQUEST['filter']; }
-        else {$filter = "Filter..."; };
-        $mode = 'read'; // mode = edit, read, NULL @todo: get this from session (conditional)
-        $request = $this->container->get('request');
-        $routeid = $request->get('_route'); // the current url from the request
+     // test for actions
+        if (isset($_REQUEST['action'])) { $act = $_REQUEST['action']; }
+        else { $act = NULL; }; // $act = NULL, add, modify, delete, save, cancel, filter
 
      // render /home/ page
+        $paramservice = $this->get('tixi_homepage_service');
         return $this->render(
             'TixiHomeBundle:Default:index.html.twig',
-            array('application' => $application, 'version' => $version, 'customer' => $customer,
-                  'breadcrumbs' => $breadcrumbs, 'username' => $username, 'subject' => $subject,
-                  'filter' => $filter, 'baseurl' => $baseurl, 'mode' => $mode,
-                  'routeid' => $routeid)
+            $paramservice->getTemplateParameters('home', 'Startseite der Dispo-Software')
         );
     }
 }
---
+++ src/Tixi/HomeBundle/Controller/HomepageController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jonasse
+ * Date: 21.08.13
+ * Time: 16:09
+ */
+// src/Tixi/HomeBundle/Controller/HomepageController.php
+
+namespace Tixi\HomeBundle\Controller;
+
+// use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+// Class HomepageController extends Controller
+// not needed at the moment: simple and fast
+
+Class HomepageController
+{
+    public function showAction($_route) {
+        var_dump($_route);
+    }
+
+    public function getTemplateParameters( $title, $subject, $errormsg='', $content='')
+    {
+    // set the parameters array for the default home page environment, some are constants, some variables ...
+    // call this every time you render the home template, this is implemented as a symfony2 service
+
+    // set common parameters
+        $title = 'iTixi cloud computing - '.$title;
+        $baseurl = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['SCRIPT_NAME']; // root url
+
+    // set parameters for the top frame
+        $application = 'iTixi'; // @todo: get this from the session
+        $version = '2.0.4'; // @todo: get this from the session
+        $customer = 'Undefined'; // @todo: get this from the session
+        $breadcrumbs = 'Breadcrumbs (@todo)'; // solve this function point
+        $username = 'Anonym'; // @todo: get this from the session
+
+    // set parameters for the menubar
+    //  $baseurl
+
+    // set parameters for the subject
+    //  $subject
+
+    // set parameters for the taskbar
+        if (isset($_REQUEST['filter'])) { $filter = $_REQUEST['filter']; } else {$filter = "Filter..."; };
+        $mode = 'read'; // mode = edit, read, NULL @todo: get this from the session
+
+    // set parameters for the error message ($errormsg = NULL, no error message)
+
+    // set parameters for the content ($content = NULL, no content)
+
+    return array(
+        'title' => $title, 'baseurl' => $baseurl,
+        'application' => $application, 'version' => $version, 'customer' => $customer,
+        'breadcrumbs' => $breadcrumbs, 'username' => $username, 'subject' => $subject,
+        'filter' => $filter, 'mode' => $mode,
+        'errormsg' => $errormsg, 'content' => $content);
+    }
+}
