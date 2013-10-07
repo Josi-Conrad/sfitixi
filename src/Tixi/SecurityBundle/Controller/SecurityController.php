4796cf3bd1052fde7c58883ccb6d7285a9af5141
Martin Jonasse <jonasse@datazug.ch>
2013-10-07 09:35:09 +0200

    Reworked login form and error message. Unit test completed.

--- src/Tixi/SecurityBundle/Controller/SecurityController.php
+++ src/Tixi/SecurityBundle/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 // src/Tixi/SecurityBundle/Controller/SecurityControlle.php
 // 26.08.2013 martin jonasse initial file, credit the book - chapter 13 - security
+// 07.10.2013 martin jonasse simplified design, fixed error message
 
 namespace Tixi\SecurityBundle\Controller;
 
@@ -11,29 +12,31 @@ use Symfony\Component\Security\Core\SecurityContext;
 class SecurityController extends Controller
 {
     public function loginAction()
-    {
-    //  @todo: add the remember_me function
+    {/*
+      * login to the iTixi application with username (name@company.ch)
+      * and password (which will be encrypted with salt)
+      */
+        // initialize page
+        $tixi_housekeeping = $this->get('tixi_housekeeping');
+        $tixi_housekeeping->setTemplateParameters('tixi_login');
 
+        // set subject
         $request = $this->getRequest();
         $session = $request->getSession();
+        //$session = $this->container->get('session');
+        $session->set('subject', 'Anmelden an der iTixi Applikation');
 
-        // get the login error if there is one
+      // get the login error if there is one
         if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(
-                SecurityContext::AUTHENTICATION_ERROR
-            );
+            $error = $request->attributes->get( SecurityContext::AUTHENTICATION_ERROR );
         } else {
             $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
             $session->remove(SecurityContext::AUTHENTICATION_ERROR);
         }
 
-        return $this->render(
-            'TixiSecurityBundle:Security:login.html.twig',
-            array(
-                // last username entered by the user
-                'last_username' => $session->get(SecurityContext::LAST_USERNAME),
-                'error'         => $error,
-            )
-        );
+        if ($error!= Null) {
+            $session->set('errormsg', 'Fehler bei der Anmeldung (Benutzername und/oder Passwort falsch)');
+        }
+        return $this->render( 'TixiSecurityBundle:Security:login.html.twig' );
     }
 }
