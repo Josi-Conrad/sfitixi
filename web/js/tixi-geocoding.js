f2637013825f1f7b59dfbd4acdd7063fa278b3b1
Martin Jonasse <jonasse@datazug.ch>
2014-01-27 19:24:59 +0100

    Rebuild of Google Maps. No longer as picture objects in forms, but as modal popups (enlarged to max size 640x640 pixel). Unit Test successfull, no open issues.

--- web/js/tixi-geocoding.js
+++ web/js/tixi-geocoding.js
@@ -12,19 +12,31 @@
 var geocoder;
 var map;
 
-function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(46.818188,8.227511999);
-    var mapOptions = {
-        zoom: 7,
+function showGoogleMap( mylat, mylng )
+{/* called as soon as an address has been successfully fetched from google */
+    var latlng = new google.maps.LatLng( mylat, mylng );
+    var myOptions = {
+        zoom: 15,
         center: latlng
-    }
-    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    };
+    if (map === undefined ) {
+        map = new google.maps.Map($('#mymap')[0], myOptions);
+    };
+    var marker = new google.maps.Marker({
+        position: latlng,
+        map: map
+    });
+    $('#mymodal').dialog({
+        width: 675,
+        height: 700,
+        modal: true
+    });
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(marker.getCurrentPosition());
 }
 
-function codeAddress() {
-    //var address = document.getElementById('address').value;
-
+function codeAddress()
+{/* called as soon as the "googlen" button is clicked */
     var myaddress = document.getElementById('fpw_strasse_nr').value + " " +
         document.getElementById('fpw_postleitzahl').value + " " +
         document.getElementById('fpw_ortsname').value + " " +
@@ -34,31 +46,26 @@ function codeAddress() {
         function(results, status)
         {
             if (status == google.maps.GeocoderStatus.OK)
-            {
-                map.setCenter(results[0].geometry.location);
-                map.setZoom(15);
-                var marker = new google.maps.Marker(
-                    {
-                        map: map,
-                        position: results[0].geometry.location
-                    });
-                /* write geocode to form element */
+            {/* write geocode to form element */
                 var gc = document.getElementById('fpw_geocode');
                 gc.value = results[0].geometry.location.lat() + ',' + results[0].geometry.location.lng();
                 if (results[0].geometry.location_type != "ROOFTOP")
                 {
                     alert("Adresse nicht präzise sondern eine Annäherung, bitte überprüfen und ändern!");
 					gc.value = results[0].geometry.location_type;
-                } else
-                {
-                    gc.value = results[0].geometry.location.lat() + ',' + results[0].geometry.location.lng();
                 }
+                showGoogleMap(results[0].geometry.location.lat(), results[0].geometry.location.lng());
             }
             else
             {
                 alert('Adresssuche mit Google Geocode war nicht erfolgreich, aus folgendem Grund: ' + status);
             }
         });
+}
+
+function initialize()
+{/* called as soon as page is loaded */
+    geocoder = new google.maps.Geocoder();
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
