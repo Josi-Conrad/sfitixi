681a29835e77a037e5ae5903d01d843f523db5b4
faustos <carmelo@carmelo.ch>
2014-04-08 22:39:57 +0200

    added shifttype sql to build

---
+++ build/shifttypes.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM tixi.shift_type
+LIMIT 0, 1000
+
+-- Date: 2014-04-08 22:02
+*/
+INSERT INTO `shift_type` (`id`,`name`,`start`,`end`) VALUES ('1','shift1.name','0000-00-00 00:00:00','0000-00-00 00:00:00');
+INSERT INTO `shift_type` (`id`,`name`,`start`,`end`) VALUES ('2','shift2.name','0000-00-00 00:00:00','0000-00-00 00:00:00');
+INSERT INTO `shift_type` (`id`,`name`,`start`,`end`) VALUES ('3','shift3.name','0000-00-00 00:00:00','0000-00-00 00:00:00');
+INSERT INTO `shift_type` (`id`,`name`,`start`,`end`) VALUES ('4','shift4.name','0000-00-00 00:00:00','0000-00-00 00:00:00');
+INSERT INTO `shift_type` (`id`,`name`,`start`,`end`) VALUES ('5','shift5.name','0000-00-00 00:00:00','0000-00-00 00:00:00');
