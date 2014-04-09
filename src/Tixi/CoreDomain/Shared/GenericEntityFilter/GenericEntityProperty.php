fc2b9f6c8851b232a9e4266b93ec0be92b484985
faustos <carmelo@carmelo.ch>
2014-04-09 23:25:53 +0200

    implemented datagrid restrictive filter capability

--- src/Tixi/CoreDomain/Shared/GenericEntityFilter/GenericEntityProperty.php
+++ src/Tixi/CoreDomain/Shared/GenericEntityFilter/GenericEntityProperty.php
@@ -13,12 +13,14 @@ class GenericEntityProperty {
     protected $entityByName;
     protected $propertyByName;
     protected $propertyValue;
+    protected $comparingOperator;
 
 
-    public function __construct($entityByName, $propertyByName, $propertyValue=null) {
+    public function __construct($entityByName, $propertyByName, $propertyValue=null, $comparingOperator='=') {
         $this->entityByName = $entityByName;
         $this->propertyByName = $propertyByName;
         $this->propertyValue = $propertyValue;
+        $this->comparingOperator = $comparingOperator;
     }
 
     /**
@@ -48,5 +50,15 @@ class GenericEntityProperty {
     public function getEntityPropertyString() {
         return $this->entityByName . '.' . $this->propertyByName;
     }
+
+    /**
+     * @return string
+     */
+    public function getComparingOperator()
+    {
+        return $this->comparingOperator;
+    }
+
+
 
 } 
