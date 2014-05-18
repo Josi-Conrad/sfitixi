ce5531ad0083399b8534bace7851c2739c44a536
Reto Schelbert <hert@hertus.ch>
2014-05-18 20:14:39 +0200

    tag 3.0, small test change

--- src/Tixi/App/AppBundle/Tests/DispositionTest.php
+++ src/Tixi/App/AppBundle/Tests/DispositionTest.php
@@ -12,8 +12,13 @@ namespace Tixi\App\AppBundle\Tests;
 use Tixi\App\Disposition\DispositionVariables;
 use Tixi\CoreDomain\Dispo\DrivingMission;
 use Tixi\CoreDomain\Dispo\DrivingOrder;
+use Tixi\CoreDomain\Dispo\DrivingPool;
 use Tixi\CoreDomain\Dispo\RepeatedDrivingAssertionPlan;
 use Tixi\CoreDomain\Dispo\RepeatedWeeklyDrivingAssertion;
+use Tixi\CoreDomain\Dispo\Shift;
+use Tixi\CoreDomain\Dispo\ShiftType;
+use Tixi\CoreDomain\Dispo\WorkingDay;
+use Tixi\CoreDomain\Dispo\WorkingMonth;
 use Tixi\CoreDomain\Passenger;
 use Tixi\CoreDomain\POI;
 use Tixi\CoreDomainBundle\Tests\CommonBaseTest;
@@ -40,7 +45,50 @@ class DispositionTest extends CommonBaseTest {
     }
 
     public function testWorkingMonthDriverAssignment() {
-        $workingMonth = $this->workingMonthRepo->findWorkingMonthByDate(new \DateTime('2014-06-01'));
+        $monthDate = new \DateTime('2014-06-01');
+        $workingMonth = $this->workingMonthRepo->findWorkingMonthByDate($monthDate);
+
+        if ($workingMonth === null) {
+            $workingMonth = WorkingMonth::registerWorkingMonth($monthDate);
+            $workingMonth->setMemo('Test');
+            $workingMonth->createWorkingDaysForThisMonth();
+
+            $shiftTypes = $this->shiftTypeRepo->findAllNotDeleted();
+
+            /**@var $workingDay WorkingDay */
+            foreach ($workingMonth->getWorkingDays() as $workingDay) {
+                $this->workingDayRepo->store($workingDay);
+                foreach ($shiftTypes as $shiftType) {
+                    $shift = Shift::registerShift($workingDay, $shiftType);
+                    $workingDay->assignShift($shift);
+                    $this->shiftRepo->store($shift);
+                }
+            }
+
+            $this->workingMonthRepo->store($workingMonth);
+            $workingDays = $workingMonth->getWorkingDays();
+
+            //create workingDays shifts, assign them drivingpools, get amount of needed drivers
+            /** @var $workingDay WorkingDay */
+            foreach ($workingDays as $workingDay) {
+                /** @var $shiftType ShiftType */
+                foreach ($shiftTypes as $shiftType) {
+                    $shift = Shift::registerShift($workingDay, $shiftType);
+                    $shift->setAmountOfDrivers(rand(12, 20));
+                    $workingDay->assignShift($shift);
+                    for ($i = 1; $i <= $shift->getAmountOfDrivers(); $i++) {
+                        $drivingPool = DrivingPool::registerDrivingPool($shift);
+                        $shift->assignDrivingPool($drivingPool);
+                        $this->drivingPoolRepo->store($drivingPool);
+                    }
+                    $this->shiftRepo->store($shift);
+                }
+                $this->workingDayRepo->store($workingDay);
+            }
+
+            $this->em->flush();
+        }
+
         $this->workingMonthManagement->assignAvailableDriversToDrivingPools($workingMonth);
         echo "\nStill not associated DrivingPools: " .
             count($this->workingMonthManagement->getAllUnassignedDrivingPoolsForMonth($workingMonth)) . "\n";
